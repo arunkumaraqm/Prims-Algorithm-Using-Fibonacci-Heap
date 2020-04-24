131ed01e2b68ca93d1ddffabecc9909e3f9f7968
unknown <arunkumar.aqm@gmail.com>
2020-04-24 14:56:16 +0530

    Switched to 0-based indexing.

--- BinaryHeapStandard.py
+++ BinaryHeapStandard.py
@@ -1,5 +1,4 @@
 class MinHeap:
-
 	def __init__(self, arr = None, already_heap = False):
 		"""
 		Creates an empty heap when passed with no parameters.
@@ -7,13 +6,15 @@ class MinHeap:
 		Calls build_heap when passed array is not a heap.
 		"""
 		if not arr:
-			self.arr = [None]
-			self.size = 0
+			self.arr = []
+
 		elif already_heap:
-			self.arr = [None] + arr # 1 based indexing
-			self.size = len(self.arr) - 1
+			self.arr = arr # 0 based indexing
+
 		else:
 			self.build_heap(arr)
+
+	def __len__(self): return len(self.arr)
 
 	def sift_down(self, ind):
 		"""
@@ -23,11 +24,11 @@ class MinHeap:
 		while True:
 			desired_child = self.find_min_child(ind)
 			if desired_child == -1: break
+
 			if self.arr[ind] > self.arr[desired_child]:
 				self.swap(ind, desired_child)
 				ind = desired_child
-			else:
-				break
+			else: break
 
 	def sift_up(self, ind):
 		"""
@@ -35,13 +36,13 @@ class MinHeap:
 		until the node is larger than its parent.
 		"""
 		while True:
-			parent = ind//2
-			if parent == 0: break
+			if ind == 0: break # Root node has no parent
+			parent = (ind-1)//2 
+
 			if self.arr[ind] < self.arr[parent]:
 				self.swap(ind, parent)
 				ind = parent
-			else:
-				break
+			else: break
 
 	def swap(self, ind1, ind2):
 		self.arr[ind1], self.arr[ind2] = self.arr[ind2], self.arr[ind1]
@@ -51,162 +52,56 @@ class MinHeap:
 		Returns the index of the smaller child of a node.
 		Returns -1 if node is a leaf.
 		"""
-		left = ind * 2
+		left = ind * 2 + 1
 		right = left + 1
-		if left > self.size:
+
+		if left >= len(self):
 			return -1
-		if right > self.size:
+		if right >= len(self):
 			return left
-		if self.arr[left] < self.arr[right]:
-			return left
-		else:
-			return right
+
+		if self.arr[left] < self.arr[right]: return left
+		else: return right
 
 	def build_heap(self, arr):
-		self.arr = [None] + arr
-		self.size = len(self.arr) - 1
-		for i in range(self.size // 2, 0, -1):
+		self.arr = arr
+
+		# Performing sift down on all non-leaf nodes.
+		# Leaf nodes always occupy the latter half of the array.
+		for i in range((len(self) - 1) // 2, 0 -1, -1):
 			self.sift_down(i)
 
 	def insert(self, ele):
 		self.arr.append(ele)
-		self.size += 1
-		self.sift_up(self.size)
+		self.sift_up(len(self) - 1) # Sift Up on the last element
 
 	def decrease_key(self, ind, ele):
-		assert self.arr[ind] < ele
+		assert self.arr[ind] >= ele, "New value is greater than current value!"
 		self.arr[ind] = ele
 		self.sift_up(ind)
 
 	def extract_min(self):
 		"Removes and returns the minimum key from heap."
-		if self.size == 0: return None
-		minn = self.arr[1]
-		self.arr[1] = self.arr[self.size]
-		del self.arr[self.size]
-		self.size -= 1
+		if len(self) == 0: return None
 
-		self.sift_down(1)
+		minn = self.arr[0]
+		self.arr[0] = self.arr[-1]
+		del self.arr[-1]
+
+		self.sift_down(0)
 		return minn
 
 	def merge(self, other):	
 		"Builds a new MinHeap after combining the two existing ones."
-		return MinHeap(self.arr[1:] + other.arr[1:])
+		return MinHeap(self.arr + other.arr)
 	
 	def __str__(self):
-		return str(self.arr[1:])
+		return str(self.arr)
 
 def heapsort(arr):
 	# Descending order since we're using a min-heap
 	heap = MinHeap(arr)
-	while heap.size:
-		heap.swap(1, heap.size)
-		heap.size -= 1
-		heap.sift_down(1)
-	return heap.arr[1:]
+	size = len(arr)
+	sorted_arr = [heap.extract_min() for i in range(size)]
+	return sorted_arr
 
-
-"""
-# Tests for extract_min:
-def testing_extract_min_1():
-	heap = MinHeap([10, 20, 25, 22, 30, 25, 28], already_heap = True)
-	for i in range(1, 8):
-		print(heap.extract_min(), heap)
-
-def testing_extract_min_2():
-	heap = MinHeap([90, 90, 90], already_heap = True)
-	for i in range(3):
-		print(heap.extract_min(), heap)
-
-def testing_extract_min_3():
-	heap = MinHeap([], already_heap = True)
-	for i in range(3):
-		print(heap.extract_min(), heap)
-
-Tests for decrease_key:
-
-#1
-heap = MinHeap([10, 20, 25, 22, 30, 25, 28], already_heap = True)
-for i in range(1, 8):
-	heap.decrease_key(i, heap.arr[i] - 1)
-print(heap)
-
-#2
-heap = MinHeap([10, 20, 25, 22, 30, 25, 28], already_heap = True)
-heap.decrease_key(7, 0)
-print(heap)
-
-#3
-heap = MinHeap([10, 20, 25, 22, 30, 25, 28], already_heap = True)
-heap.decrease_key(1, 11)
-print(heap)
-
-Tests for insert:
-#1
-heap = MinHeap()
-for i in [60, 20, 10, 70, 35, 24]:
-	heap.insert(i)
-	print(heap)
-
-#2
-heap = MinHeap([10, 20, 30, 40, 50])
-for i in [40, 30, 20, 10]:
-	heap.insert(i)
-	print(heap)
-
-Tests for build_heap:
-#1
-heap = MinHeap([60, 20, 10, 70, 35, 24])
-print(heap)
-
-#2
-heap = MinHeap()
-heap.build_heap([10, 20, 30, 40, 50, 40, 30, 20, 10])
-print(heap)
-
-Tests for sift_up:
-#1
-heap = MinHeap([10, 20, 25, 22, 30, 25, 28], already_heap = True)
-for i in range(1, 7):
-	heap.sift_up(i)
-	print(heap)
-
-#2
-heap = MinHeap([10, 20, 25, 22, 30, 25, 18], already_heap = True)
-heap.sift_up(7)
-print(heap)
-
-#3
-heap = MinHeap([10, 20, 25, 22, 30, 25, 9], already_heap = True)
-heap.sift_up(7)
-print(heap)
-
-#4
-heap = MinHeap([10, 20, 25, 18, 19, 25, 28], already_heap = True)
-heap.sift_up(5)
-heap.sift_up(4)
-print(heap)
-
-Tests for sift_down:
-#1
-heap = MinHeap([10, 20, 25, 22, 30, 25, 28], already_heap = True)
-for i in range(1, 7):
-	heap.sift_down(i)
-	print(heap)
-
-#2
-heap = MinHeap([10, 20, 25, 22, 30, 25, 18], already_heap = True)
-heap.sift_down(3)
-print(heap)
-
-#3
-heap = MinHeap([10, 20, 25, 12, 30, 25, 28], already_heap = True)
-heap.sift_down(2)
-print(heap)
-
-#4
-heap = MinHeap([70, 20, 25, 22, 30, 25, 28], already_heap = True)
-print(heap)
-heap.sift_down(1)
-print(heap)
-"""
--- TestsForHeap.py
+++ TestsForHeap.py
@@ -1,103 +1,173 @@
+from BinaryHeapStandard import MinHeap, heapsort
+
+# Tests for heapsort
+def heapsort_test_1():
+	print(heapsort([]))
+def heapsort_test_2():
+	print(heapsort([0]))
+def heapsort_test_3():
+	print(heapsort([3, 2]))
+def heapsort_test_4():
+	print(heapsort([4, 2, 8, 3, 6, 1, 6, 3, 6, 30, 20, 10, -5, 7, 3]))
+def heapsort_test_5():
+	print(heapsort([3, 5, 7, 9]))
+def heapsort_test_6():
+	print(heapsort([9, 7, 5, 3]))
+
+def heapsort_test_runner():
+	for testno in range(1, 6 + 1):
+		eval("heapsort_test_" + str(testno))()
+		print()
+heapsort_test_runner()
+
 # Tests for extract_min:
-def testing_extract_min_1():
+def extract_min_test_1():
 	heap = MinHeap([10, 20, 25, 22, 30, 25, 28], already_heap = True)
+	print(f"Original: {heap}")
 	for i in range(1, 8):
 		print(heap.extract_min(), heap)
 
-def testing_extract_min_2():
+def extract_min_test_2():
 	heap = MinHeap([90, 90, 90], already_heap = True)
+	print(f"Original: {heap}")
 	for i in range(3):
 		print(heap.extract_min(), heap)
 
-def testing_extract_min_3():
+def extract_min_test_3():
 	heap = MinHeap([], already_heap = True)
 	for i in range(3):
 		print(heap.extract_min(), heap)
 
-Tests for decrease_key:
+def extract_min_test_runner():
+	for testno in range(1, 3 + 1):
+		eval("extract_min_test_" + str(testno))()
+		print()
+extract_min_test_runner()
 
-#1
-heap = MinHeap([10, 20, 25, 22, 30, 25, 28], already_heap = True)
-for i in range(1, 8):
-	heap.decrease_key(i, heap.arr[i] - 1)
-print(heap)
+#Tests for decrease_key:
 
-#2
-heap = MinHeap([10, 20, 25, 22, 30, 25, 28], already_heap = True)
-heap.decrease_key(7, 0)
-print(heap)
-
-#3
-heap = MinHeap([10, 20, 25, 22, 30, 25, 28], already_heap = True)
-heap.decrease_key(1, 11)
-print(heap)
-
-Tests for insert:
-#1
-heap = MinHeap()
-for i in [60, 20, 10, 70, 35, 24]:
-	heap.insert(i)
+def decrease_key_test_1():
+	heap = MinHeap([10, 20, 25, 22, 30, 25, 28], already_heap = True)
+	for i in range(7):
+		heap.decrease_key(i, heap.arr[i] - 1)
 	print(heap)
 
-#2
-heap = MinHeap([10, 20, 30, 40, 50])
-for i in [40, 30, 20, 10]:
-	heap.insert(i)
+def decrease_key_test_2():
+	heap = MinHeap([10, 20, 25, 22, 30, 25, 28], already_heap = True)
+	heap.decrease_key(6, 0)
 	print(heap)
 
-Tests for build_heap:
-#1
-heap = MinHeap([60, 20, 10, 70, 35, 24])
-print(heap)
-
-#2
-heap = MinHeap()
-heap.build_heap([10, 20, 30, 40, 50, 40, 30, 20, 10])
-print(heap)
-
-
-Tests for sift_up:
-#1
-heap = MinHeap([10, 20, 25, 22, 30, 25, 28], already_heap = True)
-for i in range(1, 7):
-	heap.sift_up(i)
+def decrease_key_test_3():
+	heap = MinHeap([10, 20, 25, 22, 30, 25, 28], already_heap = True)
+	heap.decrease_key(0, 11) # Supposed to throw an error
 	print(heap)
 
-#2
-heap = MinHeap([10, 20, 25, 22, 30, 25, 18], already_heap = True)
-heap.sift_up(7)
-print(heap)
+def decrease_key_test_runner():
+	for testno in range(1, 3 + 1):
+		eval("decrease_key_test_" + str(testno))()
+		print()
+decrease_key_test_runner()
 
-#3
-heap = MinHeap([10, 20, 25, 22, 30, 25, 9], already_heap = True)
-heap.sift_up(7)
-print(heap)
+#Tests for insert:
 
-#4
-heap = MinHeap([10, 20, 25, 18, 19, 25, 28], already_heap = True)
-heap.sift_up(5)
-heap.sift_up(4)
-print(heap)
+def insert_test_1():
+	heap = MinHeap()
+	for i in [60, 20, 10, 70, 35, 24]:
+		heap.insert(i)
+		print(heap)
 
-Tests for sift_down:
-#1
-heap = MinHeap([10, 20, 25, 22, 30, 25, 28], already_heap = True)
-for i in range(1, 7):
-	heap.sift_down(i)
+def insert_test_2():
+	heap = MinHeap([10, 20, 30, 40, 50])
+	for i in [40, 30, 20, 10]:
+		heap.insert(i)
+		print(heap)
+
+def insert_test_runner():
+	for testno in range(1, 2 + 1):
+		eval("insert_test_" + str(testno))()
+		print()
+insert_test_runner()
+
+
+#Tests for build_heap:
+def build_heap_test_1():
+	heap = MinHeap([60, 20, 10, 70, 35, 24])
 	print(heap)
 
-#2
-heap = MinHeap([10, 20, 25, 22, 30, 25, 18], already_heap = True)
-heap.sift_down(3)
-print(heap)
+def build_heap_test_2():
+	heap = MinHeap()
+	heap.build_heap([10, 20, 30, 40, 50, 40, 30, 20, 10])
+	print(heap)
 
-#3
-heap = MinHeap([10, 20, 25, 12, 30, 25, 28], already_heap = True)
-heap.sift_down(2)
-print(heap)
+def build_heap_test_runner():
+	for testno in range(1, 2 + 1):
+		eval("build_heap_test_" + str(testno))()
+		print()
+build_heap_test_runner()
 
-#4
-heap = MinHeap([70, 20, 25, 22, 30, 25, 28], already_heap = True)
-print(heap)
-heap.sift_down(1)
-print(heap)
+#Tests for sift_up:
+def sift_up_test_1():
+	heap = MinHeap([10, 20, 25, 22, 30, 25, 28], already_heap = True)
+	print(f"Original: {heap}")
+	for i in range(7):
+		heap.sift_up(i)
+		print(heap) # No change expected
+
+def sift_up_test_2():
+	heap = MinHeap([10, 20, 25, 22, 30, 25, 18], already_heap = True)
+	print(f"Original: {heap}")
+	heap.sift_up(6)
+	print(heap)
+
+def sift_up_test_3():
+	heap = MinHeap([10, 20, 25, 22, 30, 25, 9], already_heap = True)
+	print(f"Original: {heap}")
+	heap.sift_up(6)
+	print(heap)
+
+def sift_up_test_4():
+	heap = MinHeap([10, 20, 25, 18, 19, 25, 28], already_heap = True)
+	print(f"Original: {heap}")
+	heap.sift_up(4)
+	heap.sift_up(3)
+	print(heap)
+
+def sift_up_test_runner():
+	for testno in range(1, 4 + 1):
+		eval("sift_up_test_" + str(testno))()
+		print()
+
+sift_up_test_runner()
+
+
+#Tests for sift_down:
+def sift_down_test_1():
+	heap = MinHeap([10, 20, 25, 22, 30, 25, 28], already_heap = True)
+	print(f"Original: {heap}")
+	for i in range(len(heap)):
+		heap.sift_down(i)
+		print(heap) # No changes should occur.
+
+def sift_down_test_2():
+	heap = MinHeap([10, 20, 25, 22, 30, 25, 18], already_heap = True)
+	print(f"Original: {heap}")
+	heap.sift_down(2)
+	print(heap)
+
+def sift_down_test_3():
+	heap = MinHeap([10, 20, 25, 12, 30, 25, 28], already_heap = True)
+	print(f"Original: {heap}")
+	heap.sift_down(1)
+	print(heap)
+
+def sift_down_test_4():
+	heap = MinHeap([70, 20, 25, 22, 30, 25, 28], already_heap = True)
+	print(f"Original: {heap}")
+	heap.sift_down(0)
+	print(heap)
+
+def sift_down_test_runner():
+	for testno in range(1, 4 + 1):
+		eval("sift_down_test_" + str(testno))()
+		print()
+sift_down_test_runner()
