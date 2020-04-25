111af1920616004f2d98f83d3bd738ee0735f7a7
unknown <arunkumar.aqm@gmail.com>
2020-04-25 14:49:00 +0530

    import Graph; comments

--- LazyNaivePrims.py
+++ LazyNaivePrims.py
@@ -2,34 +2,29 @@
 Prim's implemented with adjacency matrix and traversal.
 Lazy - edges are extracted and inserted, as opposed to vertices.
 """
+from Graph import Graph
 INFINITY = float('inf')
 
-class Graph:
-	def __init__(self, nfverts):
-		self.nfverts = nfverts
-		self.mat = []
-
-	def read_adjacency_matrix(self):
-		"Reads matrix for graph from stdin."
-
-		for _ in range(self.nfverts):
-			self.mat.append([int(number) for number in input().split()])
-
-	def fill_with_zeros(self):
-		mat = []
-		for _ in range(self.nfverts):
-			mat.append([0] * self.nfverts)
-		self.mat = mat
-
-	def __str__(self):
-		return "\n".join(str(row) for row in self.mat)
-
 class EdgeNode:
+	"""
+	A node in EdgeList representing a single edge.
+	Each edge has a "from vertex", a "to vertex" and an edge weight.
+
+	Note: 
+		Yes, an edge in an undirected graph has no direction,
+		but you can think of it as two edges -
+		an edge from u to v with weight w
+		and an edge from v to u with weight w.
+		But we'll always only need one of these edges 
+		because the from_vx is visited.
+	"""
+
 	def __init__(self, from_vx, to_vx, weight):
 		self.from_vx = from_vx
 		self.to_vx = to_vx
 		self.weight = weight
 
+	# Dunder methods for comparison of edge weights
 	def __lt__(self, other): return self.weight < other.weight
 	def __le__(self, other): return self.weight <= other.weight
 	def __gt__(self, other): return self.weight > other.weight
@@ -40,19 +35,20 @@ class EdgeNode:
 		return f"({self.from_vx}, {self.to_vx}, {self.weight})"
 
 class EdgeList(list):
+	"""
+	A list of EdgeNodes. 
+	extract_min is done by traversing the list.
+	no decrease_key because it is not needed for lazy implementation.
+	"""
 
 	def extract_min(self):
-		"Returns vertex, with minimum key, that has not been visited."
-		minn, min_ind = EdgeNode(None, None, INFINITY), None
-		for node_ind, node in enumerate(self):
-			if node < minn:
-				minn = node
-				min_ind = node_ind
-		
+		# Extracts and returns minimum edge in EdgeList object.
+
+		min_ind, minn = min(enumerate(self), key = lambda tupl: tupl[1])
+
 		try: del self[min_ind]
 		except TypeError: 
-			print("Nothing to extract! Exiting.")
-			exit()
+			raise TypeError("Nothing to extract!")
 		
 		return minn
 
@@ -62,62 +58,63 @@ class EdgeList(list):
 	def __str__(self):
 		return "[{}]".format(", ".join(str(ele) for ele in self))
 
-def adjacent_vertices_of(u_vertex, graph):
-	"Generator returning adjacent vertices of a given vertex."
-	for v_vertex, edge_cost in enumerate(graph.mat[u_vertex]):
-		if edge_cost > 0:
-			yield v_vertex
 
-
-def prims_mst(graph):
-	precursor = [None] * graph.nfverts
-	visited = [False] * graph.nfverts
+def prims_mst(grf):
+	precursor = [None] * grf.nfverts # precursor[from_vx] := to_vx for vertices in MST
+	visited = set()
 	edge_list = EdgeList()
 
+	def adjacent_vertices_of(u_vertex):
+		# Generator returning adjacent vertices of a given vertex.
+		
+		for v_vertex, edge_cost in enumerate(grf.graph[u_vertex]):
+			if edge_cost > 0:
+				yield v_vertex
+
 	def insert_useful_edges_to_edge_list(cur_vx):
-		for other_vx in adjacent_vertices_of(cur_vx, graph):
-			if visited[other_vx] == False:
-				new_node = EdgeNode(cur_vx, other_vx, graph.mat[cur_vx][other_vx])
+		# Non-zero edges from cur_vx to unvistied vertices are "useful".
+
+		for other_vx in adjacent_vertices_of(cur_vx):
+			if other_vx not in visited:
+				new_node = EdgeNode(cur_vx, other_vx, grf.graph[cur_vx][other_vx])
 				edge_list.insert(new_node)
 
 	src = 0
-	visited[src] = True
+	visited.add(src)
 	precursor[src] = None
 
 	insert_useful_edges_to_edge_list(src)
 
-	for _ in range(graph.nfverts - 1): # We need (|V| - 1) edges for a spanning tree.
+	for _ in range(grf.nfverts - 1): # We need (|V| - 1) edges for a spanning tree.
 		while True:
 			popped_node = edge_list.extract_min()
 			from_vx, cur_vx, weight = popped_node.from_vx, popped_node.to_vx, popped_node.weight 
 
-			if visited[cur_vx] == True: # This means there this edge will form a cycle in mst.
+			if cur_vx in visited: # This means there this edge will form a cycle in mst.
 				continue # So we need to choose a different edge.
 			else: 
 				break
 
-		visited[cur_vx] = True
+		visited.add(cur_vx)
 		precursor[cur_vx] = from_vx
 
 		insert_useful_edges_to_edge_list(cur_vx)
 
-	#print(f"key = {key}")
-	#print(f"precursor = {[i + 1 for i in precursor]}")
 	return precursor
 
-def compute_mst_and_cost(precursor, graph):
-	"Makes the adjacency matrix of the minimum spanning tree,\
+def compute_mst_and_cost(precursor, grf):
+	"Makes the adjacency graphrix of the minimum spanning tree,\
 	 returns that graph and also returns the total cost of the MST."
 	
-	mst = Graph(graph.nfverts)
+	mst = Graph(grf.nfverts)
 	mst.fill_with_zeros()
 	cost = 0
 
 	# precursor[0] is useless.
 	for cur, parent in enumerate(precursor[1:], 1):
 		#print(f"(cur, par) = ({cur, parent})")
-		mst.mat[parent][cur] = graph.mat[parent][cur]
-		cost += graph.mat[parent][cur]
+		mst.graph[parent][cur] = grf.graph[parent][cur]
+		cost += grf.graph[parent][cur]
 	
 	return mst, cost
 
@@ -125,11 +122,11 @@ def compute_mst_and_cost(precursor, graph):
 def main():
 	"Driver function."
 	nfverts = int(input())
-	graph = Graph(nfverts)
-	graph.read_adjacency_matrix()
+	grf = Graph(nfverts)
+	grf.read_from_stdin()
 	
-	precursor = prims_mst(graph)
-	mst, cost = compute_mst_and_cost(precursor, graph)
+	precursor = prims_mst(grf)
+	mst, cost = compute_mst_and_cost(precursor, grf)
 	
 	print(mst)
 	print(f"Cost = {cost}")
