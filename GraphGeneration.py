63f71d39906f3de4db187dc6352e56a9d6d87ecd
Arun Kumar <arunkumar.aqm@gmail.com>
2020-05-09 01:29:40 +0530

    Takes input from user now.

--- GraphGeneration.py
+++ GraphGeneration.py
@@ -1,21 +1,26 @@
-# Program to generate a test case for Prim's and Krukal's algorithms
+# Program to generate a test case (adjacency matrix) for Prim's and Krukal's algorithms
 # The graph generated will be connected.
+
 from random import shuffle, sample, randint
+import sys
 
-# modify these two values for generating desired graph
-nfverts = 200 # noof vertices
-lim = 999 # maximum edge weight
-rint = lambda: randint(1, lim)
+def read():
+	nfverts = int(input("Number of vertices: "))
+	density = int(input("Density (Medium 1, High 2 or Complete 3): "))
+	limit = float(input("Upper limit of edge weight: "))
+	limit = int(limit) if int(limit) else limit
+	return nfverts, density, limit
 
-def print_mat(mat):
-	# Printing the test case
-	print(nfverts)
-	for i in mat[1:]:
-		for j in i[1:]:
-			print("{:02d}".format(j), end = " ")
-		print()
+def print_mat(nfverts, mat, fname):
+	with open(fname, 'w') as fil:
+		fil.write(f"{nfverts}\n")
 
-def generate_random_spanning_tree(mat):
+		for i in mat[1:]:
+			for j in i[1:]:
+				fil.write(f"{j} ")
+			fil.write("\n")
+
+def generate_random_spanning_tree(nfverts, mat, rint):
 	mylist = list(range(1, nfverts + 1))
 	shuffle(mylist)
 	visited = [mylist[0]]
@@ -25,22 +30,32 @@ def generate_random_spanning_tree(mat):
 		mat[v][u] = mat[u][v]
 		visited += [v]
 
-# random_weight is 0 three out of five times 
-# and it is a random int two out of five times
-random_weight = lambda: [rint(), rint(), rint(), rint(), rint()][randint(0, 4)]
-
-def add_some_more_edges(mat):
+def add_some_more_edges(nfverts, mat, probability_distro_func):
 	for i in range(1, nfverts + 1 - 1):
 		for j in range(i + 1, nfverts + 1):
 			if not mat[i][j]:
-				mat[i][j] = random_weight()
+				mat[i][j] = probability_distro_func()
 				mat[j][i] = mat[i][j]
 				
+
+nfverts, density, limit = read()				
 mat = [[0 for i in range(nfverts + 1)] \
           for j in range(nfverts + 1)]
 
-generate_random_spanning_tree(mat)
-add_some_more_edges(mat)       
-print_mat(mat)
+rint = lambda: randint(1, limit)
+random_weight_lambdas = {
+
+	# density: random_weight_lambda	
+	1: lambda: [rint(), 0, 0][randint(0,2)],
+	2: lambda: [rint(), rint(), 0][randint(0,2)],
+	3: lambda: [rint(), rint(), rint()][randint(0,2)]
+}
+probability_distro_func = random_weight_lambdas[density]
+
+generate_random_spanning_tree(nfverts, mat, rint)
+add_some_more_edges(nfverts, mat, probability_distro_func)     
+
+fname = f"Tests/Graph Test ({nfverts}, {density}, {limit}).txt"  
+print_mat(nfverts, mat, fname)
 	
 
