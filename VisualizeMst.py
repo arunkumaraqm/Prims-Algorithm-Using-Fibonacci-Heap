8bd31808c0b47340d09df5454536a1e357855d1e
Arun Kumar <arunkumar.aqm@gmail.com>
2020-05-10 03:43:35 +0530

    Work on user interface

--- VisualizeMst.py
+++ VisualizeMst.py
@@ -4,7 +4,7 @@ import numpy as np
 from Graph import Graph as myGraph
 from sys import stdin
 
-def visualize(matrix, edges_in_mst):
+def visualize(matrix, edges_in_mst, strip_edge_weights = False):
 	
 	grf = nx.from_numpy_matrix(np.array(matrix))
 
@@ -14,8 +14,10 @@ def visualize(matrix, edges_in_mst):
 	nx.draw_networkx_nodes(grf, pos, node_color = 'pink')
 	nx.draw_networkx_labels(grf, pos)
 	nx.draw_networkx_edges(grf, pos, edge_color = 'purple', width = 1.5) # show all edges, thin lines
+	
 	nx.draw_networkx_edges(grf, pos, edgelist = edges_in_mst, edge_color = 'red', width = 2.5) # highlight mst
-	nx.draw_networkx_edge_labels(grf, pos, edge_labels = edge_labels)
+	if not strip_edge_weights:
+		nx.draw_networkx_edge_labels(grf, pos, edge_labels = edge_labels)
 
 	plt.show()	
 
